0cc58a7345f8cb9c9f577b366360eb6f8f86af35
ecrum19 <elicrum88@gmail.com>
2026-08-10 10:29:58 +0200

    2025 swat4hcls biohackathon paper record addition

--- src/data/softwareReleases.ts
+++ src/data/softwareReleases.ts
@@ -8,7 +8,7 @@ export interface SoftwareReleaseInfo {
   publishedAt: string | null;
 }
 
-export const softwareReleaseLastUpdatedIso: string | null = "2026-08-06T08:45:32.980Z";
+export const softwareReleaseLastUpdatedIso: string | null = "2026-08-10T08:23:12.533Z";
 
 export const softwareReleasesBySoftwareId: Record<string, SoftwareReleaseInfo | null> = {
   "genome-sharing-paper-webpage": {
